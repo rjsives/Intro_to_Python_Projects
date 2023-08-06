fb37c9813fc9e828cd25508790ed25d7936bc438
Rich Ives <rjsives89@gmail.com>
2023-08-05 21:35:30 -0500

    Added individual statements of weight and rounded total values

--- Family_Weight_On_Mars.py
+++ Family_Weight_On_Mars.py
@@ -1,13 +1,15 @@
 #Create a program to calculate weights on Mars for your entire family. The function should ask for each family member's weight, calculate how much they would weigh on Mars (by multiplying the number by 0.3782) and then add up and display the total weight.
 
-family = ["Kelly", "Rich", "Lexi", "Cybil", "Eddie", "Eli"]
-moon_weight_conversion = 0.3782
+#Define's a list of family members
+family = ["Kelly", "Rich", "Lexi", "Cybil", "Clem", "Eddie", "Eli", "Marble", "Jeffrey"]
+mars_weight_conversion = 0.3782
 
 def calculate_family_weight():
   total_weight = 0
   for person in family:
     weight = int(input(f"Enter {person}'s weight:"))
-    total_weight = total_weight + weight*0.3782
-  return total_weight
+    print(f"{person} weighs {weight} lbs on Earth: {round(weight * mars_weight_conversion)} lbs on Mars. \n")
+    total_weight = total_weight + weight * mars_weight_conversion
+  return round(total_weight)
 
-print(calculate_family_weight())
+print(f"Our family's total weight on Mars is {(calculate_family_weight())} lbs.")
