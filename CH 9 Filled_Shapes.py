30172c2f9830819a0da31cb21f41147d53572a0b
Rich Ives <rjsives89@gmail.com>
2023-08-13 13:51:03 -0500

    Filled Shapes Tutorial added function to create filled circles of various colors and sizes

--- CH 9 Filled_Shapes.py
+++ CH 9 Filled_Shapes.py
@@ -40,5 +40,45 @@ t.setheading(0)
 t.begin_fill()
 t.circle(10)
 t.end_fill()
+#Demostrate creating a filled yellow circle
+#sets the position
+t2 = turtle.Turtle()
+t2.up()
+t2.right(90)
+t2.forward(50)
+#sets the color and draws the circle
+t2.color(1,1,0)
+t2.begin_fill()
+t2.circle(20)
+t2.end_fill()
+#Create a function to draw a filled circle
+t3 = turtle.Turtle()
+t3.up()
+t3.left(90)
+t3.forward(100)
+t3.down()
+def myCircle(red, green, blue, size):
+    t3.color(red, green, blue)
+    t3.begin_fill()
+    t3.circle(size)
+    t3.end_fill()
+#Draw a large gold circle using the function
+myCircle(0.9,0.75,0,50)
+#Draw medium light pink circle
+myCircle(1,.7,.75,30)
+#Draw small orange circle
+myCircle(1,.5,0,15)
+#Draw tiny light taupe circle
+myCircle(.9,.5,.15,7)
+#Experiment with mixing your own colors
+t3.up()
+t3.right(90)
+t3.forward(100)
+t3.down()
+
+myCircle(0.5,0.5,0,50)
+myCircle(0.5, 0.5, 0.75, 30)
+myCircle(0, 0.75, .5, 15)
+myCircle(0, 0, 0.9, 7)
 
 turtle.done()
