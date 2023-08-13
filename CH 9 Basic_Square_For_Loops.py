7417926b9edb10c3e51d0b1fcd4ef0963cddbbfa
Rich Ives <rjsives89@gmail.com>
2023-08-13 09:44:35 -0500

    For Loops Used to Create Basic Squares with Turtle Module
    recode to display three concentric squares

--- CH 9 Basic_Square_For_Loops.py
+++ CH 9 Basic_Square_For_Loops.py
@@ -6,18 +6,33 @@ u = turtle.Turtle()
 
 #turte.reset deletes this turtle's drawings and restores it's default values
 t.reset()
-for x in range(1,5):
+for x in range(1, 5):
     t.forward(50)
     t.left(90)
 
 s.reset()
-for x in range(1,5):
+s.penup()
+s.left(180)
+s.forward(50)
+s.left(90)
+s.forward(50)
+s.left(90)
+s.pendown()
+for x in range(1, 5):
     s.forward(150)
     s.left(90)
 
+
 u.reset()
+u.penup()
+u.left(180)
+u.forward(125)
+u.left(90)
+u.forward(125)
+u.left(90)
+u.pendown()
 for x in range(1, 5):
     u.forward(300)
     u.left(90)
 
-#turtle.done()
+turtle.done()
