0d232d46ec4a45068b200208ef3c1d9456cc01cf
Rich Ives <rjsives89@gmail.com>
2023-08-14 22:12:11 -0500

    Filled Shapes Tutorial create a function draw a filled (or not filled) star with parameters size, color, filled

--- CH 9 Drawing_Stars.py
+++ CH 9 Drawing_Stars.py
@@ -17,7 +17,6 @@ t.pendown()
 t.pencolor("dodgerblue")
 t.speed(0)
 
-
 for x in range(1, 39):
     t.forward(100)
     t.left(175)
@@ -33,7 +32,6 @@ t.speed(0)
 for x in range(1, 20):
     t.forward(100)
     t.left(95)
-
 
 t.reset
 t.speed(10)
@@ -58,7 +56,7 @@ t2.left(90)
 t2.pendown()
 t2.pencolor("greenyellow")
 
-#adding an if statement based on the condition of the iterator x being even or odd
+# adding an if statement based on the condition of the iterator x being even or odd
 for x in range(1, 19):
     t2.forward(50)
     if x % 2 == 0:
@@ -66,9 +64,9 @@ for x in range(1, 19):
     else:
         t2.left(225)
 
-#Multicolored Spirograph design using rhombus
+# Multicolored Spirograph design using rhombus
 t2.up()
-t2.right(87)
+t2.right(90)
 t2.forward(100)
 t2.down()
 
@@ -87,5 +85,36 @@ for x in range(1, 81):
         t2.pencolor("dodgerblue")
         t2.forward(10)
         t2.left(60)
+
+# Creating a filled Star using a function
+t3 = turtle.Turtle()
+t3.up()
+t3.right(90)
+t3.forward(100)
+t3.down()
+t3.speed(0)
+
+def my_star(size, color, filled):
+    t3.color(color)
+    if filled:
+        t3.begin_fill()
+    for x in range(1, 19):
+        t3.forward(size)
+        if x % 2 == 0:
+            t3.left(175)
+        else:
+            t3.left(225)
+    if filled:
+        t3.end_fill()
+
+
+my_star(100, "black", True)
+my_star(100, "red", False)
+my_star(50, "yellow", True)
+my_star(50, "red", False)
+my_star(25, "blue", True)
+my_star(25, "red", False)
+my_star(12.5, "slategrey", True)
+my_star(12.5, "red", False)
 
 turtle.done()
