6ef31b1ccbb6ebe9d7682139edf50d7f0f8de798
Rich Ives <rjsives89@gmail.com>
2023-08-16 18:24:35 -0500

    Three Approaches to Generating Stars
    Three star making functions combined in a function designed to print all three.
    progressive stars demonstrate progressively adding more points to the stars.  Currently complicated_star is producting the correct number of points on even numbered stars

--- CH 9 Star Creator Challenge.py
+++ CH 9 Star Creator Challenge.py
@@ -1,53 +1,82 @@
+import time
 import turtle
 
 t1 = turtle.Turtle()
-t1.up()
+"""t1.up()
 t1.left(180)
 t1.forward(300)
-t1.down()
+t1.down()"""
 
 
 def star_maker(size, points):
     double_points = points * 2
-    angle1 = 180 - (360/points)
-    angle2 = angle1 * 3
-
+    angle1 = 180 - (360/double_points)
+    angle2 = (angle1 * 3)
     for x in range(0, double_points):
+        t1.speed(0)
         t1.forward(size)
         if x % 2 == 0:
             t1.left(angle1)
         else:
-            t1.left(angle2)
+            t1.right(angle2)
 
-star_maker(50, 7)
-
+# star_maker(50, 7)
 
 def star_designer(size, points):
     angle = 360 / points
     for x in range(0, points):
+        t1.speed(0)
         t1.forward(size)
         t1.left(180 - angle)
         t1.forward(size)
         t1.right(180 - (angle * 2))
 
-t1.up()
+def complicated_star(size, points):
+    double_points = points * 2
+    angle1 = 180 - (360/points)
+    angle2 = (angle1 * 3)
+    for x in range(0, double_points):
+        t1.speed(0)
+        t1.forward(size)
+        if x % 2 == 0:
+            t1.left(angle1)
+        else:
+            t1.right(angle2)
+
+"""t1.up()
 t1.left(180)
 t1.forward(300)
 t1.down()
 
-star_designer(50, 5)
+# star_designer(50, 5)
 
 t1.up()
 t1.forward(300)
-t1.down()
+t1.down()"""
 
 
 def double_star(size, points):
+    turtle.resetscreen()
+    t1.speed()
     star_maker(size, points)
+    t1.left(180)
+    t1.up()
+    t1.forward(350)
+    t1.down()
     star_designer(size, points)
+    t1.left(180)
+    t1.up()
+    t1.forward(700)
+    t1.down()
+    complicated_star(size, points)
+    time.sleep(6)
 
-double_star(55, 31)
+def progressive_stars():
+    for x in range (5, 61):
+        double_star(50, x)
 
+progressive_stars()
+"""
 def shape_maker(size, points):
     angle = 360/points
     for x in range(1, points + 1):
@@ -61,7 +90,7 @@ shape_maker(50, 6)
 shape_maker(50, 7)
 shape_maker(50, 8)
 shape_maker(50, 9)
-shape_maker(50, 10)
+shape_maker(50, 10)"""
 
 
 
